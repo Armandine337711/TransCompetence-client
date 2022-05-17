63ed24a4df46fb74582c0abbace4f0e3790e99ee
steeve mellet <melletsteeve@orange.fr>
2022-05-17 16:20:09 +0200

    Mise en place des headers de chaque utilisateur

---
+++ src/pages/SalesHomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SalesHeader from '../components/SalesHeader';
+import Footer from '../components/Footer.js'
+
+const SalesHomePage = () => {
+  return (
+    <div>
+      <SalesHeader />
+      <Footer />
+    </div>
+  );
+};
+
+export default SalesHomePage;
