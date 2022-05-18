6e7b6ac52b8544e441b4eec34a25a98f28808911
Bruno <brunoschmit@yahoo.fr>
2022-05-18 14:43:54 +0200

    modification financialhomepages pour onglets

--- src/pages/FinancialHomePage.js
+++ src/pages/FinancialHomePage.js
@@ -1,47 +1,14 @@
 import React from 'react';
-import DataPackage1 from '../components/DataPackage1';
-import DataPackage2 from '../components/DataPackage2';
-import DataPackage3 from '../components/DataPackage3';
-import DataPackage4 from '../components/DataPackage4';
-import DataPackage5 from '../components/DataPackage5';
-import DataPackage6 from '../components/DataPackage6';
-import DataPackage7 from '../components/DataPackage7';
-import DataPackage8 from '../components/DataPackage8';
-import DataPackage9 from '../components/DataPackage9';
-import DataPackage10 from '../components/DataPackage10';
-import DataPackage11 from '../components/DataPackage11';
-import DataPackage12 from '../components/DataPackage12';
-import DataPackage13 from '../components/DataPackage13';
-import DataPackage14 from '../components/DataPackage14';
-import DataPackage15 from '../components/DataPackage15';
-import DataPackage16 from '../components/DataPackage16';
-import DataPackage17 from '../components/DataPackage17';
-import DataPackage18 from '../components/DataPackage18';
 import FinancialMenu from '../components/FinancialMenu'
+import Onglets from '../components/Onglets';
 import Footer from '../components/Footer'
 
 const FinancialHomePage = () => {
   return (
     <div>
       <FinancialMenu />
-      <DataPackage1/>
-      <DataPackage2/>
-      <DataPackage3/>
-      <DataPackage4/>
-      <DataPackage5/>
-      <DataPackage6/>
-      <DataPackage7/>
-      <DataPackage8/>
-      <DataPackage9/>
-      <DataPackage10/>
-      <DataPackage11/>
-      <DataPackage12/>
-      <DataPackage13/>
-      <DataPackage14/>
-      <DataPackage15/>
-      <DataPackage16/>
-      <DataPackage17/>
-      <DataPackage18/>        
+      <Onglets/>
+        
       <Footer />
     </div>
   );
