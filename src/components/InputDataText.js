da6f1a3134746460fec07dd8ca39fc567250d9d2
Bruno <brunoschmit@yahoo.fr>
2022-05-18 10:12:46 +0200

    ajout de inputdataText pour la valeur E5 qui nécessite du texte + modification de la page FinancialHomePage

---
+++ src/components/InputDataText.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class InputDataText extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            label:'',
+            ref: '',
+            value: ''
+        };
+    };
+    render(){
+        return(
+            <div className='Input-data'>
+			  <label>{this.props.label} </label>
+			  <input 
+                type="Text" 
+                value={this.state.value}
+                placeholder={this.props.placeholder}
+                onChange={(event) => this.setState({value: event.target.value })}
+              />
+			</div>
+        )
+    }
+}
+export default InputDataText;
