805aeaf49b9d816343a012cc02d00fd645a3e156
Armandine BARGE <armandine.barge@ntymail.com>
2022-05-26 12:06:28 +0200

    setup state login

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,43 +1,77 @@
-import React from 'react';
+import { useState } from 'react';
 import '../style/style.css'
 
 const Connection = () => {
+  const API_URL = process.env.REACT_APP_API_URL
+  const [loginUser, setLoginUser] = useState({
+    login: '',
+    password: ''
+  });
+  // const [password, setPassword] = useState('');
+
+  const handleChange = (event) => {
+    console.log(event)
+    const name = event.currentTarget.name;
+    const value = event.currentTarget.value;
+    setLoginUser(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${API_URL}/login`,
+      {
+        method: "POST",
+        body: JSON.stringify(loginUser),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+  };
+
   return (
     <div className="ConnectionPage">
       <h1 className="ConnectionTitle">CONNEXION</h1>
-      <form 
-      action=""
-      method="post"
-      className="FormConnection">
-        <div 
-        className='ConnectionUser'>
-          <label 
-          for="user"
-          className="LabelUser">Identifiant : 
+      <form
+        action=""
+        method="post"
+        className="FormConnection"
+        onSubmit={handleSubmit}>
+
+        <div
+          className='ConnectionUser'>
+          <label
+            htmlFor="user"
+            className="LabelUser">Identifiant :
           </label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             id="user"
+            name="login"
             placeholder="Identifiant"
+            value={loginUser.login}
+            onChange={handleChange}
           ></input>
         </div>
-         <br/>
-         <div 
-         className='ConnectionPassword'>
-          <label 
-          for="password"
-          className="PasswordUser">Mot de passe : 
+        <br />
+        <div
+          className='ConnectionPassword'>
+          <label
+            htmlFor="password"
+            className="PasswordUser">Mot de passe :
           </label>
-          <input 
-            type="password" 
+          <input
+            type="password"
             id="password"
+            name='password'
             placeholder="Mot de passe"
+            value={loginUser.password}
+            onChange={handleChange}
           ></input>
-          <br/>
-         </div>
-         <div
-         className="ConnectionValidate">
-          <button 
+          <br />
+        </div>
+        <div
+          className="ConnectionValidate">
+          <button
             type="submit"
             className="ConnectionButton"
           >Valider
